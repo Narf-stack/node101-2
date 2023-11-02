f6a9945d4c5387e62c9f93924ded9a3bef8a5ab4
Narf-stack <fran.stacksta@gmail.com>
2023-11-02 16:38:35 +0100

    Express demo - params handling v2

--- express-demo/app.js
+++ express-demo/app.js
@@ -2,19 +2,26 @@ const express  = require('express')
 const app = express()
 
 
+const courses = [
+  {id: 1, name: 'courses1' },
+  {id: 2, name: 'courses2' },
+  {id: 3, name: 'courses3' },
+]
 // define routes
 app.get('/',(req,res) => {
   res.send('hello world')
 })
 
 app.get('/api/courses',(req,res) => {
-  res.send([1,2,3])
+  res.send(courses)
 })
 
 app.get('/api/courses/:id',(req,res) => {
-  
-  //res.send(`course ${req.params.id}`)
-  res.send(`query param ${req.query}`)
+  const course = courses.find(c => c.id === parseInt(req.params.id))
+  if(!course) res.status(404).send(`course ${req.params.id} not found`)
+  res.send(course)
+  // res.send(`course ${req.params.id}`)
+  // res.send(`query param ${req.query}`)
 })
 
 // Assign port value 
