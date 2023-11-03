cf347743f79a552b261f068e1a62f55c29f21891
Narf-stack <fran.stacksta@gmail.com>
2023-11-03 17:58:12 +0100

    Node express/express tuto - http headers basic

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,8 +1,10 @@
 const http = require('http')
+const { writeHeapSnapshot } = require('v8')
 
 const server = http.createServer((req,res)=>{
-  console.log('hit server')
-  res.end('hello wlrd')
+  res.writeHead(200, {"content-type": "text/html"})
+  res.write('<h1> hello wlrd</h1>')
+  res.end()
 })
 
 server.listen(5000)
