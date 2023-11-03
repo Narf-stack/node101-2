e31eda3ce4debe3dec2b00a86ca1c096964cd430
Narf-stack <fran.stacksta@gmail.com>
2023-11-03 17:48:17 +0100

    Node express/express tuto - http basic

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+  console.log('hit server')
+  res.end('hello wlrd')
+})
+
+server.listen(5000)
