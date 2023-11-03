087330579b38f08c7b2341a39212461b5ee1f2f9
Narf-stack <fran.stacksta@gmail.com>
2023-11-03 18:19:23 +0100

    Node express/express tuto - request object

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -2,9 +2,25 @@ const http = require('http')
 const { writeHeapSnapshot } = require('v8')
 
 const server = http.createServer((req,res)=>{
-  res.writeHead(200, {"content-type": "text/html"})
-  res.write('<h1> hello wlrd</h1>')
-  res.end()
+  const url = req.url
+  // home page
+  if(url=== '/') {
+    res.writeHead(200, {"content-type": "text/html"})
+    res.write('<h1> hello wlrd</h1>')
+    res.end()
+  }
+  // about page
+  else if(url === '/about'){
+    res.writeHead(200, {"content-type": "text/html"})
+    res.write('<h1> abouttte</h1>')
+    res.end()
+  }
+  // 404
+  else {
+    res.writeHead(404, {"content-type": "text/html"})
+    res.write('<h1> error</h1>')
+    res.end()
+  }
 })
 
 server.listen(5000)
