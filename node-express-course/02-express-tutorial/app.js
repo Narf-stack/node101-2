e3c5bd56616a54e20f931d1250e199322f68c302
Narf-stack <fran.stacksta@gmail.com>
2023-11-04 18:11:25 +0100

    Node express/express tuto - navbar in  static asset

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -5,9 +5,10 @@ const app = express()
 // set up static and middleware
 app.use(express.static('./public'))
 
-app.get('/', (req,res) =>{
-  res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-})
+// app.get('/', (req,res) =>{
+//   res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// render in static file
+// })
 
 app.all('*',(req,res)=>{
   res.status(404).send('not found')
