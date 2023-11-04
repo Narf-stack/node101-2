01b8af7ae2b7239693509a17dc193b11ce65d978
Narf-stack <fran.stacksta@gmail.com>
2023-11-04 17:47:27 +0100

    Node express/express tuto - express basic + static asset

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,51 +1,17 @@
-const http = require('http')
-const {readFileSync} = require('fs')
-const { writeHeapSnapshot } = require('v8')
+const express = require('express')
+const path = require('path')
+const app = express()
 
+// set up static and middleware
+app.use(express.static('./public'))
 
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
-
-const server = http.createServer((req,res)=>{
-  const url = req.url
-  // home page
-  if(url=== '/') {
-    res.writeHead(200, {"content-type": "text/html"})
-    res.write(homePage)
-    res.end()
-  }
-  // styles
-  else if(url === '/styles.css'){
-    res.writeHead(200, {"content-type": "text/css"})
-    res.write(homeStyles)
-    res.end()
-  }
-   // iamge
-   else if(url === '/logo.svg'){
-    res.writeHead(200, {"content-type": "image/svg+xml"})
-    res.write(homeImage)
-    res.end()
-  }
-   // logic
-   else if(url === '/browser-app.js'){
-    res.writeHead(200, {"content-type": "text/javascript"})
-    res.write(homeLogic)
-    res.end()
-  }
-  // about page
-  else if(url === '/about'){
-    res.writeHead(200, {"content-type": "text/html"})
-    res.write('<h1> abouttte</h1>')
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, {"content-type": "text/html"})
-    res.write('<h1> error</h1>')
-    res.end()
-  }
+app.get('/', (req,res) =>{
+  res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
 })
 
-server.listen(5000)
+app.all('*',(req,res)=>{
+  res.status(404).send('not found')
+})
+app.listen(5000,()=> {
+  console.log('server listening')
+})
