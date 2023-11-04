5ba6ed09fd76a4063823da303ba768bafe37722e
Narf-stack <fran.stacksta@gmail.com>
2023-11-04 16:33:40 +0100

    Node express/express tuto - html basic

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,12 +1,37 @@
 const http = require('http')
+const {readFileSync} = require('fs')
 const { writeHeapSnapshot } = require('v8')
+
+
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 const server = http.createServer((req,res)=>{
   const url = req.url
   // home page
   if(url=== '/') {
     res.writeHead(200, {"content-type": "text/html"})
-    res.write('<h1> hello wlrd</h1>')
+    res.write(homePage)
+    res.end()
+  }
+  // styles
+  else if(url === '/styles.css'){
+    res.writeHead(200, {"content-type": "text/css"})
+    res.write(homeStyles)
+    res.end()
+  }
+   // iamge
+   else if(url === '/logo.svg'){
+    res.writeHead(200, {"content-type": "image/svg+xml"})
+    res.write(homeImage)
+    res.end()
+  }
+   // logic
+   else if(url === '/browser-app.js'){
+    res.writeHead(200, {"content-type": "text/javascript"})
+    res.write(homeLogic)
     res.end()
   }
   // about page
