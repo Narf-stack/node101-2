19d3036078e9a43853753e8b3e7756b1cd7ccd4f
Narf-stack <fran.stacksta@gmail.com>
2023-11-05 12:37:26 +0100

    Node express/express tuto - routes params

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -5,8 +5,34 @@ const { products, people } = require('./data')
 
 
 app.get('/', (req,res) =>{
-  res.json(people)
+  res.send('<h1>welcom wrld</h1><a href="/api/products">prod</a>')
 })
+
+app.get('/api/products', (req,res) =>{
+  const newProds = products.map((product) =>{
+    const {id,name,image} = product
+    return  {id,name,image}
+  })
+  res.json(newProds)
+})
+
+
+app.get('/api/products/:id', (req,res) =>{
+  // Classique
+  // const singleProd = products.find((product)=>{
+  //   return product.id === parseInt(req.params.id)
+  // })
+
+  // one liner
+  // const singleProd = products.find(product => product.id === parseInt(req.params.id))
+
+  // destructur
+  const singleProd = products.find(({id}) => id === parseInt(req.params.id))
+  
+  if(!singleProd) return res.status(404).json({error: `prod ${req.params.id} not found`})
+  res.json(singleProd)
+})
+
 
 app.all('*',(req,res)=>{
   res.status(404).send('not found')
