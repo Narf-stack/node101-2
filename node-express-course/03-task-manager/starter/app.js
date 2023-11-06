81204e05bfdf22a46e6bf010b7932da7b7956879
Narf-stack <fran.stacksta@gmail.com>
2023-11-06 11:23:07 +0100

    Node express/task manager - routes structure

--- node-express-course/03-task-manager/starter/app.js
+++ node-express-course/03-task-manager/starter/app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+// routes
+app.get('/hello',(req,res)=>{
+  res.status(200).send('task manager app')
+})
+
+
+const port = 3000
+
+app.listen(port, () =>{
+  console.log('server listenning')
+  
+})
