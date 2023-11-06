50869d931b19575d8d2d5968e22135d63b4144c0
Narf-stack <fran.stacksta@gmail.com>
2023-11-06 17:19:05 +0100

    Node express/task manager - schema basic validation

--- node-express-course/03-task-manager/starter/models/Task.js
+++ node-express-course/03-task-manager/starter/models/Task.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose')
 
 // Creation of our data schema 
 const TaskSchema = new mongoose.Schema({
-  name:String,completed: Boolean
+  name:{
+    // Validators
+    type: String,
+    required:[true, 'must provide name'],
+    trim:true,
+    maxlength: [10, 'name can not be more than 10 characters'],
+    minlength: [3, 'name can not be less than 3 characters'],
+  },
+  completed:{
+    // Validators
+    type: Boolean,
+    default: false
+  }
 })
 
 // Export of our data model - (mongoose.model(ModelNameSingular, ModelNameSchema)
