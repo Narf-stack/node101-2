0719d7ff220b2bf5877d82c1dd72353a6ae799ae
Narf-stack <fran.stacksta@gmail.com>
2023-11-07 15:31:44 +0100

    Node express/store api - dynamicaly search with param in DB

--- node-express-course/04-store-api/starter/controllers/products.js
+++ node-express-course/04-store-api/starter/controllers/products.js
@@ -1,12 +1,26 @@
-
+const Product = require('../models/product')
 
 
 const getProductsStatic = async (req,res) =>{
-  res.status(200).json({msg:'all products testing routes'})
+  const products = await Product.find({})
+  res.status(200).json({nbhits: products.length, status: 'success', data:products})
 }
 
 const getProducts = async (req,res) =>{
-  res.status(200).json({msg:'all products'})
+  const { featured, company, name } = req.query
+  const queryObject = {}
+
+  if(featured){
+    queryObject.featured = featured ==='true' ? true : false
+  }
+  if(company){
+    queryObject.company = company
+  }
+  if(name){
+    queryObject.name ={$regex: name, $options: 'i'}
+  }
+  const products = await Product.find(queryObject)
+  res.status(200).json({nbhits: products.length, status: 'success', data:products})
 }
 
 
