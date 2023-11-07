371916c79e0b01e53dd9a582904140ee06c25f6b
Narf-stack <fran.stacksta@gmail.com>
2023-11-07 16:01:11 +0100

    Node express/store api - dynamicaly limit , page seach result with param in DB

--- node-express-course/04-store-api/starter/controllers/products.js
+++ node-express-course/04-store-api/starter/controllers/products.js
@@ -7,7 +7,7 @@ const getProductsStatic = async (req,res) =>{
 }
 
 const getProducts = async (req,res) =>{
-  const { featured, company, name, sort } = req.query
+  const { featured, company, name, sort, fields } = req.query
   const queryObject = {}
 
   if(featured){
@@ -21,12 +21,25 @@ const getProducts = async (req,res) =>{
   }
 
   let result = Product.find(queryObject)
+  // sort 
   if(sort) {
     const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
   } else {
     result = result.sort('createdAt')
   }
+  // filter fields 
+  if(fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  } 
+  
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page -1) * limit
+
+  result = result.skip(skip).limit(limit)
+
   const products = await result
   res.status(200).json({nbhits: products.length, status: 'success', data:products})
 }
