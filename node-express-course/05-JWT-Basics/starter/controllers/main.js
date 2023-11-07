a59ac02ed710e0032a34fa1494db121172f9b47f
Narf-stack <fran.stacksta@gmail.com>
2023-11-07 17:56:06 +0100

    Node express/JWT basics - basic token creation

--- node-express-course/05-JWT-Basics/starter/controllers/main.js
+++ node-express-course/05-JWT-Basics/starter/controllers/main.js
@@ -1,7 +1,32 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
 
 
 const login = async (req,res)=> {
-  res.status(200).send('fake loggin signup route')
+  const { username, password} = req.body
+
+  // How check if credentials are provided 
+  // Option 1 : Use Mongoose validation and check with the DB
+  // Option 2 : Use Joi validation to check input for put request
+  // Option 3 : Check in controller right away 
+  if(!username || !password){
+   throw new CustomAPIError('provide login credentials',400) 
+  }
+
+  // Since no DB connection, we create a fake ID
+  const id = new Date().getDate()
+
+  // try to keep the payload small, better UX. If big payload, it will be more heavy to receive if bad internet connection, 
+  const token = jwt.sign(
+    {
+      id,
+      username
+    },
+    process.env.JWT_SECRET,
+    {expiresIn:'30d'}
+  )
+  res.status(200).json({msg:'user created', token})
 }
 
 const dashboard = async (req,res) => {
