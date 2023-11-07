14c16dd98f55888f7aaf6cbb31d4119ab56fd501
Narf-stack <fran.stacksta@gmail.com>
2023-11-07 19:01:45 +0100

    Node express/JWT basics - use library http-status-codes

--- node-express-course/05-JWT-Basics/starter/errors/bad-request.js
+++ node-express-course/05-JWT-Basics/starter/errors/bad-request.js
@@ -1,9 +1,10 @@
 const CustomAPIError = require('./custom-error')
+const {StatusCodes} = require('http-status-codes')
 
 class BadRequest extends CustomAPIError {
   constructor(message) {
     super(message)
-    this.statusCide = 400
+    this.statusCide = StatusCodes.BAD_REQUEST
   }
 }
 
