cc0ee4ce3e5ad3ffbf626fce518f71860daed166
Narf-stack <fran.stacksta@gmail.com>
2023-11-07 11:11:17 +0100

    Node express/store api - basic express server

--- node-express-course/04-store-api/starter/app.js
+++ node-express-course/04-store-api/starter/app.js
@@ -1,1 +1,39 @@
-console.log('04 Store API')
+// invoc .env file
+require('dotenv').config()
+
+//ASYNC ERRORS
+
+// APP
+const express = require('express')
+const app = express()
+// MIDDLEWARES
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(express.json()) // parse json
+
+// rootes
+ app.get('/', (req,res)=>{
+  res.status(200).send('<h1>Store API</h1><a href="/app/v1/products">products route </a>')
+ })
+
+// products route
+
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+const start = async () => {
+  try {
+    // enclose to connect to DB before listenning 
+    app.listen(port, () =>{
+      console.log( `server listenning ${port}`)
+    })
+  } catch (error) {
+    errorHandlerMiddleware(error)
+  }
+}
+
+
+start()
