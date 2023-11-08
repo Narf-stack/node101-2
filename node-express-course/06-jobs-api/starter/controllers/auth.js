6ef92891469132a947a7af2c821987d135d4b410
Narf-stack <fran.stacksta@gmail.com>
2023-11-08 18:30:54 +0100

    Node express/Jobs api - register User / register user compare password

--- node-express-course/06-jobs-api/starter/controllers/auth.js
+++ node-express-course/06-jobs-api/starter/controllers/auth.js
@@ -19,11 +19,18 @@ const login = async (req,res) => {
   }
 
   const user = await User.findOne({email:email})
-
   if(!user){
     throw new UnauthenticatedError('Invalid Credentials')
   }
 
+  // compare password
+  const isPasswordCorrect = await user.checkPassword(password)
+
+  if(!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+
+  // send JWT token
   const token = user.createJWT()
   res.status(StatusCodes.OK).json({ user:{name: user.name}, token})
 }
--- node-express-course/06-jobs-api/starter/models/User.js
+++ node-express-course/06-jobs-api/starter/models/User.js
@@ -38,6 +38,11 @@ UserSchema.pre('save', async function(){
   this.password = await bcrypt.hash(this.password, salt)
 })
 
+UserSchema.methods.checkPassword = async function (candidatePassword){
+  const isMatch = await bcrypt.compare(candidatePassword, this.password) 
+  return isMatch
+}
+
 UserSchema.methods.createJWT= function (){
   return jwt.sign(
     {
