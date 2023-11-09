2f4e064b418a6df07651cb2e921104cca6aff895
Narf-stack <fran.stacksta@gmail.com>
2023-11-09 17:41:03 +0100

    Node express/Jobs api - security package

--- node-express-course/06-jobs-api/starter/app.js
+++ node-express-course/06-jobs-api/starter/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xssClean = require('xss-clean')
+const expressRateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -14,8 +21,17 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
-
+// init security packages
+app.set('trust proxy',1)
+app.use(expressRateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xssClean())
 // routes
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/jobs',authenticateUser,jobsRouter)
