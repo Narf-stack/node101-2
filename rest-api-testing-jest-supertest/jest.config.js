47b466262b0d3b94704733fa8cb86c7649716dac
Narf-stack <fran.stacksta@gmail.com>
2023-11-15 12:06:15 +0100

    rest-api-testing-jest-supertest - jest config file v2

--- rest-api-testing-jest-supertest/jest.config.js
+++ rest-api-testing-jest-supertest/jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ["**/**/*.test.ts"],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  clearMocks:true
 };
