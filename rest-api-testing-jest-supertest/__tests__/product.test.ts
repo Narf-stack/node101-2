34a9dc947a6e5e4c48f83274eacffb1d90f8eaf2
Narf-stack <fran.stacksta@gmail.com>
2023-11-16 09:21:08 +0100

    remove rest-api-testing-jest-supertest

--- rest-api-testing-jest-supertest/__tests__/product.test.ts
+++
@@ -1,9 +0,0 @@
-describe('product',()=>{
-  describe('get product route',()=>{
-    describe('given the product does not exist',()=>{
-      describe('it should return a 404 ',() => {
-
-      })
-    })
-  })
-})
