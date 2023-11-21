a34b9b43986f7714d0acf0bad8bdf48f93e5cfd2
Narf-stack <fran.stacksta@gmail.com>
2023-11-21 12:42:46 +0100

    api-testing-jest-supertest- session model

---
+++ api-ts-jest-supertest/src/models/session.model.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+import { UserDocument } from './user.model'
+
+
+// Typescript definition for the sessionSchema 
+export interface sessionDocument extends mongoose.Document {
+  user: UserDocument['_id'],
+  valid: boolean,
+  userAgent: string,
+  createdAt: Date,
+  updatedAt: Date 
+}
+
+
+// Mongoose used to define this before mongoose 6. For backward's compatibility, we will now just define it ourselves.
+interface HookNextFunction {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (error?: Error): any
+}
+
+// sessionSchema
+const sessionSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please provide user'],
+    unique: true,
+  },
+  valid: {
+    type: Boolean,
+    default: true
+  },
+  userAgent: {
+    type: String
+  }
+}, {
+  timestamps:true
+});
+
+
+// session model
+const SessionModel = mongoose.model('Session', sessionSchema)
+
+export default SessionModel
